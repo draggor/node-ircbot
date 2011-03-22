a8c628e830ad56e2867a2923fc244fb589daceb1
Draggor <draggor@gmail.com>
2011-03-22 16:37:09 -0500

    comma fail

--- plugins/dice/index.js
+++ plugins/dice/index.js
@@ -1,15 +1,23 @@
 var plugin = require('../../plugin')
-  , dice = require('./dice');
+  , dice = require('./dice')
+  ;
 
 var regex = /\[[\dd\+\*\-\/\.\(\)]*\]/g;
 
 function parseLine(from, to, msg) {
 	var exprs = msg.match(regex);
 	if(exprs) {
-		var rolls = exprs.map(rollDice),
-		  , say = from + ': ';
+		var rolls = exprs.map(rollDice)
+		  , say = from + ': '
+		  ;
 		for(var i = 0; i < rolls.length; i++) {
-			say += rolls[i][0] + ' => ' + rolls[i][1][1] + ' => ' + rolls[i][1][0] + ' <|> ';
+			say += rolls[i][0]
+			     + ' => '
+			     + rolls[i][1][1]
+			     + ' => '
+			     + rolls[i][1][0]
+			     + ' <|> '
+			     ;
 		}
 		dicep.bot.client.say(to, say);
 	}
